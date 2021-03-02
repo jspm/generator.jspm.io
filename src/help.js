2d5a3db81a051f887d04ba1a9cd5eea841fe84c6
Guy Bedford <guybedford@gmail.com>
2021-03-02 14:06:46 +0200

    implement help popup

--- src/help.js
+++ src/help.js
@@ -1,5 +1,11 @@
 import { popup } from './popup.js';
 
 document.querySelector('.help').addEventListener('click', () => {
-  popup(`<p>There is no help!</p>`, 800, 600);
+  popup(`
+    <p>For full example workflows, see the <a href="https://jspm.org/docs/workflows" target="_blank">JSPM Workflows documentation</a>.</p>
+    <p>To load an example import map, have a look at the <a href="#U2VgYGBiDkpNTC5RCC5JLCpJLWIoAvF0U/JzHQzN9Qz0DCECUA4AS5lz8DEA" onclick="import('./src/popup.js').then(popup => popup.close())">React Import Map Example</a>.</p>
+    <p>For information on the output options, see the <a href="https://jspm.org/docs/cdn#jspm-generator" target="_blank">JSPM Generator documentation</a>.</p>
+    <p>The animation below demonstrates how dependencies can be added and managed, "Add Dependency" supports versions and subpaths. Each dependencies line corresponds to a package version and exports subpath which directly corresponds to an entry in the import map.</p>
+    <img src="jspm-generator.gif" style="margin-left: auto; margin-right: auto; display: block; box-shadow: 0px 0px 10px rgb(0 0 0 / 10%); border-radius: 2px; height: 350px;"/>
+  `, 800, 600);
 });
--- src/popup.js
+++ src/popup.js
@@ -1,30 +1,32 @@
+let curPopup, curShim;
 
-
+export function close () {
+  if (!curPopup) return;
+  curShim.removeEventListener('click', close);
+  curPopup.querySelector('.close').removeEventListener('click', close);
+  document.body.removeChild(curShim);
+  document.body.removeChild(curPopup);
+  curPopup = curShim = null;
+}
 
 export function popup (content, width = 500, height = 500) {
-  const shim = Object.assign(document.createElement('div'), { className: 'shim' });
-  const popup = Object.assign(document.createElement('div'), {
+  if (curPopup) close();
+  curShim = Object.assign(document.createElement('div'), { className: 'shim' });
+  curPopup = Object.assign(document.createElement('div'), {
     className: 'popup-container',
     innerHTML: `<div class="popup"><div class="close"></div><div class="content">${content}</div></div>`
   });
-  const popupInner = popup.querySelector('.popup');
+  const popupInner = curPopup.querySelector('.popup');
   popupInner.style.width = width + 'px';
   popupInner.style.height = height + 'px';
   popupInner.style.marginLeft = -(width / 2) + 'px';
   popupInner.style.marginTop = -(height / 2) + 'px';
-  shim.addEventListener('click', closePopup);
-  popup.querySelector('.close').addEventListener('click', closePopup);
+  curShim.addEventListener('click', close);
+  curPopup.querySelector('.close').addEventListener('click', close);
 
-  function closePopup () {
-    shim.removeEventListener('click', closePopup);
-    popup.querySelector('.close').removeEventListener('click', closePopup);
-    document.body.removeChild(shim);
-    document.body.removeChild(popup);
-  }
+  document.body.appendChild(curShim);
+  document.body.appendChild(curPopup);
 
-  document.body.appendChild(shim);
-  document.body.appendChild(popup);
-
-  setTimeout(() => shim.style.opacity = 0.7, 0);
-  setTimeout(() => popup.style.display = 'block', 100);
+  setTimeout(() => curShim.style.opacity = 0.7, 0);
+  setTimeout(() => curPopup.style.display = 'block', 100);
 }
